80cfe97d86445b894a2fc03ff6d4375c6fb69a53
manishthani <manishthani@users.noreply.github.com>
2025-09-18 17:14:15 +0200

    Adding base setup for vcpkg and cmake

---
+++ GameEngineTemplate.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+int main()
+{
+	glm::mat4 rotationMatrix(1.0f);
+	
+	std::cout << "Hello CMake and vcpkg." << std::endl;
+	return 0;
+}
