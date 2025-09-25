e49a4b38d7e089aeb3842a2e4eac58ac3db80c6e
manishthani <manishthani@users.noreply.github.com>
2025-09-25 21:06:39 +0200

    Setting up an SDL&OpenGL Window

--- GameEngineTemplate.cpp
+++ GameEngineTemplate.cpp
@@ -1,13 +1,150 @@
 ﻿#include <iostream>
 
+// OpenGL
+#include <glad/glad.h>
+#include <SDL3/SDL.h>
+
+// GLM
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+// ImGui
+#include "imgui.h"
+#include "imgui_impl_sdl3.h"
+#include "imgui_impl_opengl3.h"
+
 int main()
 {
-	glm::mat4 rotationMatrix(1.0f);
-	
-	std::cout << "Hello CMake and vcpkg." << std::endl;
-	return 0;
+    // Window Resolution
+    const int SCREEN_WIDTH = 1920;
+    const int SCREEN_HEIGHT = 1080;
+
+    // Init SDL
+    if (!SDL_Init(SDL_INIT_VIDEO))
+    {
+        return -1;
+    }
+
+    // Setup Min/Major version for using OpenGL 4
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 5);
+
+    // Set Core Profile Mode
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+    // Create OpenGL window using SDL
+    SDL_Window* window = SDL_CreateWindow("EnginishGL",
+        SCREEN_WIDTH, SCREEN_HEIGHT,
+        SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE
+    );
+
+    // Early out if window not valid
+    if (window == nullptr)
+    {
+        SDL_Quit();
+        return -1;
+    }
+
+    // OpenGL is context based and thread local
+    // Link OpenGL context to SDL, after this you can load OpenGL functions and start rendering
+    // Multi-threading -> Define multiple context and make them current
+    SDL_GLContext glContext = SDL_GL_CreateContext(window);
+    if (!glContext)
+    {
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return -1;
+    }
+
+    // Init all OpenGL function pointers at runtime (not linked at compile time)
+    gladLoadGL();
+
+    // Used for mapping NDC coordinates (-1.0f to 1.0f) to pixel coordinates (e.g. 1920x1080)
+    glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    // IMGUI
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplSDL3_InitForOpenGL(window, glContext);
+    ImGui_ImplOpenGL3_Init();
+
+    bool isRunning = true;
+    while (isRunning)
+    {
+        // INPUT
+        // SDL EVENTS
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            ImGui_ImplSDL3_ProcessEvent(&event);
+            if (event.type == SDL_EVENT_QUIT)
+            {
+                isRunning = false;
+            }
+
+            if (event.type == SDL_EVENT_MOUSE_BUTTON_UP 
+                && event.button.button == SDL_BUTTON_RIGHT)
+            {
+            }
+
+            if ((event.type == SDL_EVENT_MOUSE_BUTTON_DOWN 
+                && event.button.button == SDL_BUTTON_RIGHT))
+            {
+            }
+
+        }
+
+        // SDL KEYS
+        int numKeys;
+        const bool* keys = SDL_GetKeyboardState(&numKeys);
+        if (keys[SDL_SCANCODE_W])
+        {
+        }
+
+        // UPDATE
+        // [...]
+
+        // RENDER
+        // Clear screen color
+        glClearColor(0.1f, 0.2f, 0.2f, 1.0f);
+
+        // Clear Color Buffer and Depth Buffer
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // RENDER
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplSDL3_NewFrame();
+        ImGui::NewFrame();
+
+        // Show sample demo from ImGui
+        ImGui::ShowDemoWindow();
+
+        // Render ImGui
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        // Swap window
+        SDL_GL_SwapWindow(window);
+
+        // FRAME CONTROL
+        // [...]
+    }
+
+    // Deinit ImGui
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplSDL3_Shutdown();
+    ImGui::DestroyContext();
+
+    // DeInit SDL
+    SDL_GL_DestroyContext(glContext);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
 }
