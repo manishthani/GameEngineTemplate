a423b44560857d69429776f250102e763932668a
manishthani <manishthani@users.noreply.github.com>
2025-09-25 21:25:33 +0200

    Draw triangle using index buffer (EBO)

--- GameEngineTemplate.cpp
+++ GameEngineTemplate.cpp
@@ -115,15 +115,28 @@ int main()
     {
         -0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
         0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
-        0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f
+        0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f,
+        -0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,
+        0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,
+        0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f
     };
 
-    // Create VAO & VBO
+    GLuint indices[] =
+    {
+        0, 3, 5,
+        3, 2, 4,
+        5, 4, 1
+    };
+
+    // Create VAO & VBO & EBO
     GLuint VAO;
     glGenVertexArrays(1, &VAO);
 
     GLuint VBO;
     glGenBuffers(1, &VBO);
+
+    GLuint EBO;
+    glGenBuffers(1, &EBO);
 
     // Bind VAO and VBO
     glBindVertexArray(VAO);
@@ -131,6 +144,10 @@ int main()
     // Link GL_ARRAY_BUFFER to vertices data
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Link GL_ELEMENT_ARRAY_BUFFER to indices data
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // Define Vertex layout and set attribute index
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
@@ -190,7 +207,7 @@ int main()
         glBindVertexArray(VAO);
 
         // Specify primitive type and vertex count
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
 
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplSDL3_NewFrame();
@@ -213,6 +230,7 @@ int main()
     // Delete VAO, VBO and shader program
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glDeleteProgram(shaderProgram);
 
     // Deinit ImGui
